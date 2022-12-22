4f7087a8021c6022b3a62ad33f8c8d6e4363744e
udithr11 <udithr11@gmail.com>
2022-12-22 18:28:14 +0530

    mongodb update

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,13 @@ import pymongo
 import pandas as pd
 import json
 
+from dotenv import load_dotenv
+print(f"Loading environment variable from .env file")
+load_dotenv()
+
+
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+client = pymongo.MongoClient("mongodb+srv://udithr:Ammazz123@cluster0.vqc1j4v.mongodb.net/?retryWrites=true&w=majority")
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
